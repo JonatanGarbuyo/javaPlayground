165d1d93cb21565b5dde7c47a896a65d5e2ce034
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-09-26 11:49:06 -0300

    Ejercicios tema 3

---
+++ src/Ejercicios_tema_3.java
@@ -0,0 +1,24 @@
+public class Ejercicios_tema_3 {
+
+    public static void main(String[] args){
+        System.out.println(suma(1 ,2, 4) );
+
+        Coche miCoche = new Coche();
+        miCoche.incrementar_puertas();
+        System.out.println(miCoche.numero_de_puertas);
+
+    }
+
+    public static int suma (int a, int b, int c){
+        return a + b + c;
+    }
+
+}
+
+class Coche {
+    int numero_de_puertas = 2;
+
+    public void incrementar_puertas(){
+        this.numero_de_puertas++;
+    }
+}
